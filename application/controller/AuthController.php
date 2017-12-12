dcaedcbc5f015b5542769aca62aa0182ad84bfe6
YamiShadow <mathieu.nibas@gmail.com>
2017-12-12 19:04:02 +0100

    Suppression old class Création des views maj controllers

--- application/controller/AuthController.php
+++ application/controller/AuthController.php
@@ -1,23 +1,23 @@
 <?php 
-
-
 switch($action){
     case 'login':
         echo 'login';
-
         $data['title'] = 'lol';
 
-
-
-
+        view('auth/'$action);
         break;
     case 'login_form':
         echo 'login_form';
         break;
-
-
-
-
+    case 'register':            // Renvoie le formulaire pour register
+        view('auth/'.$action); 
+        break;
+    case 'register_form':       // Traite le formulaire
+        require_once('models/users.class.php');
+        $user = new User();
+        $user->nom = $_POST['nom'];
+        $user->prenom = $_POST['prenom'];
+        //header location 
+    default: 
+        die('Erreur de routing dans AuthController.');
 }
-
-view($action);
---
+++ application/views/auth/register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inscription</title>
+</head>
+<body>
+    <form action="index.php?action=register_form">
+        <input type="text" name="login" id="login" placeholder="Login">
+        <input type="password" name="mdp" id="mpd" placeholder="mot de passe">
+        <input type="password" name="confirmMdp" id="confirmMdp" placeholder="confirmation du mot de passe">
+        <input type="submit" value="Connexion">
+    </form>
+</body>
+</html>
