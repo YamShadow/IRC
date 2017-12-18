6a88f37e9c19531df437564b855eec177148cee8
florianrambur <flo.rambur@gmail.com>
2017-12-18 19:24:59 +0100

    Popup et ajout de serveur, popup des paramètres utilisateurs, popup des profils utilisateurs

--- application/views/chat/chat.php
+++ application/views/chat/chat.php
@@ -1,45 +1,94 @@
-<main class="window">
-    <div class="bar">
-        <div class="btn"></div>
-    </div>
-    <div class="body">
-        <pre>
-            <aside id="salonsList">
-                <h2 class="comment">#Salons :</h2>
-                <h3 class="custom">$ <span class="greenColor">add array Salons</span></h3>
-                <ul>
-                    <li v-for="salon in salons">{{ salon.text }}</li>
-                </ul>
-                
-                <button class="btn-shell addSalon">Ajouter un salon</button>
-            </aside>
+  <main class="window">
+      <div class="bar">
+          <div class="btn"></div>
+          <i title="Paramètres" class="fa fa-cog" aria-hidden="true"></i>
+      </div>
+      <div class="body">
+          <pre>
+              <aside id="salonsList">
+                  <h2 class="comment">#Salons :</h2>
+                  <h3 class="custom">$ <span class="greenColor">add array Salons</span></h3>
 
-            <object data="http://localhost:3000?pseudo=<?= $_SESSION['user_pseudo'] ?>&salon=room" width="1000" height="1000">
-                <p>Le serveur Node n'est pas lancé !</p>
-            </object>
+                  <vue-salon ref="foo"></vue-salon>
 
-            <aside id="connectedMembers">
-            <h2 class="comment">#Connectés :</h2>
-                <ul>
-                    <li v-for="member in members">{{ member.text }}</li>
-                </ul>
-            </aside>
-        </pre>
-    </div>
-</main>
+                  <button class="btn-shell addSalon">Ajouter un salon</button>
+              </aside>
 
-<div id="modalAddSalon">
+              <object data="http://localhost:3000?pseudo=<?= $_SESSION['user_pseudo'] ?>&salon=room" width="1000" height="1000">
+                  <p>Le serveur Node n'est pas lancé !</p>
+              </object>
+
+              <aside id="connectedMembers">
+              <h2 class="comment">#Connectés :</h2>
+              <h3 class="custom">$ <span class="greenColor">echo each Members</span></h3>
+                  <ul>
+                      <li v-for="member in members" :id="member.id" :pseudo="member.pseudo" :avatar="member.avatar">{{ member.pseudo }}</li>
+                  </ul>
+              </aside>
+          </pre>
+      </div>
+  </main>
+  <div class="filter">
+</div>
+
+<div id="modalAddSalon" class="modal">
     <div class="window">
         <div class="bar">
-            <div class="btn"></div>
+            <div class="btn">
+              <span class="closeModal">x</span>
+            </div>
         </div>
         <div class="body">
             <pre>
                 $ <span class="greenColor" data-typer="add input newSalon"></span>
-                <input class="pulse input-shell" type="text" v-model="name" />
-                <button class="btn-shell" v-on:click="addSalon">Ajouter</button>
+                <input class="pulse input-shell" id="inputNewSalon" type="text" v-model="name" />
+                <p class="errorMessage">Le salon doit avoir un nom !</p>
+                <button class="btn-shell" id="addNewSalon">Ajouter</button>
             </pre>
         </div>
     </div>
-    
+</div>
+
+<div id="modalParameters" class="modal">
+    <div class="window">
+        <div class="bar">
+            <div class="btn">
+              <span class="closeModal">x</span>
+            </div>
+        </div>
+        <div class="body">
+            <pre>
+                <h2 class="comment">#Modifier les paramètres de l'utilisateur :</h2>
+                <div class="prompt">$ <span class="command">add input update Login</span></div>
+                <div class="prompt">Update Login</div>
+                <div class="prompt">$ <input type="text" name="login" id="login" class="pulse input-shell" required></div>
+                <div class="prompt">$ <span class="command">add input update password</span></div>
+                <div class="prompt">Update Password</div>
+                <div class="prompt">$ <input type="password" name="mdp" id="mdp" class="pulse input-shell" required></div>
+                <div class="prompt">$ <span class="command">add input update confirmPassword</span></div>
+                <div class="prompt">Update Confirm Password</div>
+                <div class="prompt">$ <input type="password" name="confirmMdp" id="confirmMdp" class="pulse input-shell" required></div>
+                <div class="prompt">$ <span class="command">add input update image</span></div>
+                <div class="prompt">Update Url Avatar</div>
+                <div class="prompt">$ <input type="text" name="image" id="image" class="pulse input-shell" ></div>
+                <div><button type="submit" class="btn-shell">Valider les changements</button></div>
+            </pre>
+        </div>
+    </div>
+</div>
+
+<div id="modalUsers" class="modal">
+  <div class="window">
+    <div class="bar">
+      <div class="btn">
+        <span class="closeModal">x</span>
+      </div>
+    </div>
+    <div class="body">
+      <pre>
+        Profil de <span data-id="" class="profilPseudo"></span>
+        <img class="profilAvatar" src="" alt="" />
+      </pre>
+    </div>
+  </div>
 </div>
--- application/views/dom/header.php
+++ application/views/dom/header.php
@@ -1,4 +1,4 @@
-<!doctype html> 
+<!doctype html>
 <html lang="fr">
 <head>
     <title><?php if (isset($header['title'])){echo $header['title'];}?></title>
@@ -7,9 +7,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Playfair+Display:400,700|Roboto:400,700" rel="stylesheet">
     <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
+    <link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' />
     <link rel='stylesheet' type='text/css' href='assets/css/reset.css' />
 
-    <?php 
+    <?php
     if (isset($header['css'])){
         foreach ($header['css'] as $fichier) {
             echo "<link rel='stylesheet' type='text/css' href='assets/css/".$fichier.".css' />\n";
