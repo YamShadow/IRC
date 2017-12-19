dc8755459538e5ae24e18a3a0b984e9d844eef85
2alheure <jojo77.juv@live.fr>
2017-12-19 09:27:51 +0100

    Update du TODO. ajax incomming

---
+++ ajax.php
@@ -0,0 +1,22 @@
+<?php 
+session_start();
+
+define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+require_once('system/core.php');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
